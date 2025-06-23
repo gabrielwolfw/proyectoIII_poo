2a4d2bcb811f00cf6dea635e850d214f7ab75ca7
Mishelle <mishelleorellanajimenez@gmail.com>
2025-06-23 14:24:09 -0600

    2PANTALLAS

---
+++ POOIII/src/main/java/com/mycompany/pooiii/POOIII.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pooiii;
+
+/**
+ *
+ * @author mishe
+ */
+public class POOIII {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
