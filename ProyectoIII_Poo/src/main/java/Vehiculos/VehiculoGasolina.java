8c5471132370835f73343828e1b4816c4208ad66
Nightmare <isaacag204@gmail.com>
2025-06-23 23:08:28 -0600

    agregados de vuelta

---
+++ ProyectoIII_Poo/src/main/java/Vehiculos/VehiculoGasolina.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Vehiculos;
+
+/**
+ *
+ * @author isaac
+ */
+public class VehiculoGasolina extends Vehiculo{
+     private Combustible tipoGasolina;
+
+    public VehiculoGasolina(Combustible tipoGasolina, String modelo, int placa) {
+        super(modelo, placa);
+        this.tipoGasolina = tipoGasolina;
+    }
+
+    public Combustible getTipoGasolina() {
+        return tipoGasolina;
+    }
+
+    public void setTipoGasolina(Combustible tipoGasolina) {
+        this.tipoGasolina = tipoGasolina;
+    }
+}
